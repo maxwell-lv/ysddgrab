847463365f02b2a4c1b5dedf3caf8ce3779ab955
maxwell.lv <maxwell.lv@outlook.com>
2017-07-10 17:10:56 +0800

    init

---
+++ ysdd.py
@@ -0,0 +1,33 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+
+def init_driver():
+    driver = webdriver.Chrome()
+    driver.wait = WebDriverWait(driver, 5)
+    return driver
+
+
+def login(driver):
+    driver.get("http://www.vipysdd.com")
+    try:
+        userId = driver.wait.until(EC.presence_of_element_located((By.ID, "userId")))
+        password = driver.wait.until(EC.presence_of_element_located((By.ID, "password")))
+        button = driver.wait.until(EC.element_to_be_clickable((By.ID, "loginBtn")))
+        userId.send_keys("15652389451")
+        password.send_keys("lvqipass123")
+        button.click()
+    except TimeoutException:
+        print("Box or Button not found in google.com")
+
+
+if __name__ == "__main__":
+    driver = init_driver()
+    login(driver)
+    time.sleep(5)
+    driver.quit()
+
